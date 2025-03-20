8084e143176cae2d11cc3ad0e4d0d371f1d85049
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2025-03-20 14:42:13 -0600

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,12 @@ const aside = document.querySelector("aside");
 
 openMenu.addEventListener("click", () => {
     aside.classList.add("aside-visible");
+    openMenu.style.display = "none"; // Oculta el botón de abrir
+    closeMenu.style.display = "block"; // Muestra el botón de cerrar
 });
 
 closeMenu.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
+    openMenu.style.display = "block"; // Muestra el botón de abrir
+    closeMenu.style.display = "none"; // Oculta el botón de cerrar
 });
